ec2647e36a865e3e4740754fc1fe85c32fbb186a
Brian Yang <yangbri1@msu.edu>
2025-02-03 02:21:54 +0000

    Retrieves both 'GasEngine' class & 'NuclearEngine' class from ApplicationContext from requesting a bean of respective class using .getBean() method.

--- src/main/java/Lab/Lab.java
+++ src/main/java/Lab/Lab.java
@@ -29,13 +29,13 @@ public class Lab {
      * @return a GasEngine bean
      */
     public GasEngine getGasPoweredBean(){
-        return null;
+        return applicationContext.getBean(GasEngine.class);
     }
     /**
      * TODO: Retrieve a NuclearEngine bean and return it.
      * @return a NuclearEngine bean
      */
     public NuclearEngine getNuclearPoweredBean(){
-        return null;
+        return applicationContext.getBean(NuclearEngine.class);
     }
 }
